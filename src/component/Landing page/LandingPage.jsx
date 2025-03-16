8b96bde6fd8226549822ccddac65f3180a8af9a6
Yun4g <delightvincent487@gmail.com>
2025-03-16 14:51:24 +0100

    fixed routing issue and updated Mentee component

--- src/component/Landing page/LandingPage.jsx
+++ src/component/Landing page/LandingPage.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-// import Login from '../UserAuth/Login/Login';
-// import SignUp from '../UserAuth/register/SignUp';
-import ResetPassWord from '../UserAuth/resetPassword/resetPassword';
-import MentorDashBoard from '../MeentoDashboard/mentor-DashBoard';
-import Mentee from '../Mentee-onboarding/Mentee';
+
 
 function LandingPage() {
     return (
