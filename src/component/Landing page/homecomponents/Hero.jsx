4bc98760517d2cabc6d46437bba3d0f12a8cddc8
dominion <gendominion432@gmail.com>
2025-03-16 14:08:54 +0100

    working on heroSection

--- src/component/Landing page/homecomponents/Hero.jsx
+++ src/component/Landing page/homecomponents/Hero.jsx
@@ -6,10 +6,10 @@ const Hero = () => {
     <div className='flex items-center justify-between px-1'>
       <div>
           <ul className='grid grid-cols-2 gap-2'>
-           <img src={asset.blackman1} alt="" />
-           <img src={asset.people} alt="" />
-           <img src={asset.closeup} alt="" />
-           <img src={asset.frame17} alt="" />
+           <img className='rounded-xl' src={asset.blackman1} alt="" />
+           <img className='rounded-xl' src={asset.people} alt="" />
+           <img className='rounded-xl' src={asset.closeup} alt="" />
+           <img className='rounded-xl' src={asset.frame17} alt="" />
           </ul>
       </div>
       <div className='text-center px-4'>
@@ -17,15 +17,15 @@ const Hero = () => {
             Strategic<br/>
             Mentorship
         </h1>
-        <p>Experience life-changing mentorship, your path to extraordinary growth starts here.</p>
-        <p>what do you want to get better at</p>
+        <p className='mb-5 mt-4 p-3 text-sm'>Experience life-changing mentorship, your path to <br/> extraordinary growth starts here.</p>
+        <p className='border rounded-xl py-3'>what do you want to get better at</p>
       </div>
       <div>
           <ul className='grid grid-cols-2 gap-2'>
-           <img src={asset.young} alt="" />
-           <img src={asset.confident} alt="" />
-           <img src={asset.portrait} alt="" />
-           <img src={asset.couple } alt="" />
+           <img className='rounded-xl' src={asset.young} alt="" />
+           <img className='rounded-xl' src={asset.confident} alt="" />
+           <img className='rounded-xl' src={asset.portrait} alt="" />
+           <img className='rounded-xl' src={asset.couple } alt="" />
           </ul>
       </div>
     </div>
