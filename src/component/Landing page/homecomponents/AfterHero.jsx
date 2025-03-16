b2f64df96041ea6c1d8fe2539f1dbb92fdb64761
dominion <gendominion432@gmail.com>
2025-03-16 15:26:10 +0100

    file: afterhero updated

--- src/component/Landing page/homecomponents/AfterHero.jsx
+++ src/component/Landing page/homecomponents/AfterHero.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { asset } from '../../../assets/assets'
-
+import { CircleDotDashed } from 'lucide-react';
 
 const AfterHero = () => {
   return (
-    <div>
-      <div className='relative'>
+    <div className=''>
+      <div className='relative '>
         <img className='mt-24 h-15' src={asset.section} alt="" />
-        <div className='absolute top-7'>Your Mentor, Your Future Connect, Grow, Achieve Your Vision, Our Mission</div>
+        <div className='absolute top-7 flex items-center justify-center text-2xl  px-20 text-yellow-50 font-bold'> 
+          <CircleDotDashed className='mr-6'/>
+          Your Mentor, Your Future 
+          <CircleDotDashed className='mr-6 ml-6'/> 
+          Connect, Grow, Achieve 
+          <CircleDotDashed className='mr-6 ml-6'/> 
+          Your Vision, Our Mission
+          <CircleDotDashed className='mr-6 ml-6'/> 
+          </div>
       </div>
     </div>
   )
