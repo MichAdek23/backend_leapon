9900c651baabf8b732a231d236141829f14342d6
dominion <gendominion432@gmail.com>
2025-03-17 14:03:22 +0100

    updated hero/afterhero

--- src/component/Landing page/homecomponents/AfterHero.jsx
+++ src/component/Landing page/homecomponents/AfterHero.jsx
@@ -5,16 +5,18 @@ import { CircleDotDashed } from 'lucide-react';
 const AfterHero = () => {
   return (
     <div className=' '>
-      <div className='relative '>
-        <img className='mt-24 h-15 w-full' src={asset.section} alt="" />
-        <div className='absolute top-7 w-full flex items-center justify-center text-2xl  px-20 text-yellow-50 font-bold'> 
-          <CircleDotDashed className='mr-6'/>
+      <div className='relative pb-20'>
+        <img className='mt-20 h-15 w-full' src={asset.section} alt="" />
+        <div className='absolute md:top-4 sm:top-2 top-1 w-full flex items-center justify-center text-[10px] sm:text[16px] md:text-[24px] md:px-3 px-2 sm:px-5 text-orange-50 font-bold'> 
+          <CircleDotDashed className='w-4 mr-2 sm:mr-6 md:mr-2'/>
+          <div className='mr-2'>
           Your Mentor, Your Future 
-          <CircleDotDashed className='mr-6 ml-6'/> 
-          Connect, Grow, Achieve 
-          <CircleDotDashed className='mr-6 ml-6'/> 
-          Your Vision, Our Mission
-          <CircleDotDashed className='mr-6 ml-6'/> 
+          </div>
+          <CircleDotDashed className='w-4 mr-2 sm:mr-4 sm:ml-6'/> 
+          <div className='mr-2'>Connect, Grow, Achieve </div>
+          <CircleDotDashed className='w-4 mr-2 sm:mr-4 sm:ml-6'/> 
+          <div className='mr-2'> Your Vision, Our Mission</div>
+          <CircleDotDashed className='w-4 sm:mr-4 sm:ml-6'/> 
           </div>
       </div>
     </div>
