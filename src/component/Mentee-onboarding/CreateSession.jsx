d2fe44d62fb1b8ecdc49fd7fe348627bbbc75d20
Yun4g <delightvincent487@gmail.com>
2025-03-28 13:17:16 +0100

    hi

--- src/component/Mentee-onboarding/CreateSession.jsx
+++ src/component/Mentee-onboarding/CreateSession.jsx
@@ -319,13 +319,13 @@ const CreateSession = () => {
           )}
         </div>
         */}
-        
-    ggggggggg
+
+   
         {/* Date and Time */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div>
             <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
-              Date
+              Date  ggggggggg
             </label>
             <input
               type="date"
