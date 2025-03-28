957e3551fdab0d47dba7786fc315294bdca30f83
dominion <gendominion432@gmail.com>
2025-03-28 18:09:05 +0100

    added overlay

--- src/component/Landing page/homecomponents/Overview.jsx
+++ src/component/Landing page/homecomponents/Overview.jsx
@@ -1,24 +1,32 @@
-import { asset } from '@/assets/assets'
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import { asset } from '@/assets/assets';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Overview = () => {
+  const navigate = useNavigate();
+
   return (
-   
-    <div className="relative  mb-50 bg-[url('/beforeFooter.png')] bg-center bg-cover bg-no-repeat h-[40%] sm:h-[100%]">
-        <div className='grid grid-cols-1 text-center pt-5 sm:pt-20 gap-y-1 sm:gap-y-3'>
-            <h1 className='text-xl text-orange-50 font-bold sm:text-4xl'>Your Path to Success, Starts Here</h1>
-            <h5 className='text-orange-50 font-semibold text-[12px] sm:text-md'>Connect with Industry Leaders, Accelerate Your Career.</h5>
-            <Link onClick={()=>  navigate('/SignUp')} className=" mx-auto text-orange-500 md:block border bg-orange-50 flex border-orange-50  px-2 py-1 sm:px-6 sm:py-2 rounded-xl">Get Started</Link>
+    <div className='w-full sm:h-[100vh]'> 
+      <div className="relative mb-12 bg-[url('/Section5.png')] bg-center bg-cover w-full h-[40vh] sm:h-[90%]">
+        <div className='grid grid-cols-1 text-center pt-5 sm:pt-16 gap-y-1 sm:gap-y-3'>
+          <h1 className='text-xl text-orange-50 font-bold sm:text-4xl'>Your Path to Success, Starts Here</h1>
+          <h5 className='text-orange-50 font-semibold text-[12px] sm:text-md'>Connect with Industry Leaders, Accelerate Your Career.</h5>
+          <button
+            onClick={() => navigate('/SignUp')}
+            className="mx-auto text-orange-500 text-[10px] sm:text-sm md:block border bg-orange-50 flex border-orange-50 px-2 py-1 sm:px-6 sm:py-2 rounded-xl"
+          >
+            Get Started
+          </button>
         </div>
-      <div className='absolute bottom-0 flex mx-auto w-[80%] left-10 right-10 sm:left-20 sm:right-20 '>
-        <img src={asset.Overview} alt="" className=''/>
+        <div className='absolute bottom-0 flex mx-auto w-[80%] left-10 right-10 sm:left-20 sm:right-20 '>
+          <div className="relative w-full h-auto">
+            <img src={asset.Overview} alt="" className="block w-full" />
+            <div className="absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-b from-orange-50 to-orange-500 opacity-70"></div>
+          </div>
+        </div>
       </div>
     </div>
-    
-  )
-  
-}
+  );
+};
 
-export default Overview
+export default Overview;
