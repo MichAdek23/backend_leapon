e05f3451e9b3cbc8442da32822ec8c29b8baca09
Yun4g <delightvincent487@gmail.com>
2025-03-28 14:15:28 +0100

    enter code n

--- src/component/Mentee-onboarding/Messages.jsx
+++ src/component/Mentee-onboarding/Messages.jsx
@@ -148,10 +148,10 @@ const Messages = () => {
         <div className="flex flex-col w-full lg:flex-row justify-start items-start lg:items-center gap-4 lg:gap-0 lg:justify-between">
           <div className="flex flex-col gap-4">
             <h1 className="text-[32px] font-medium">
-              {user?.role === 'mentor' ? 'Mentees' : 'Mentors'}
+              Messages
             </h1>
             <p className="text-base font-medium text-slate-600">
-              Message
+            communication is key to success
             </p>
           </div>
 
